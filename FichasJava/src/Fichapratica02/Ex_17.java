5524d45b436482ceadf41d696b5e6def79644198
Hldsilv <helderlcpsilveira@gmail.com>
2024-09-17 22:30:08 +0100

    17 e 18
    feito em casa

--- FichasJava/src/Fichapratica02/Ex_17.java
+++ FichasJava/src/Fichapratica02/Ex_17.java
@@ -4,11 +4,53 @@ import java.util.Scanner;
 
 public class Ex_17 {
     public static void main(String[] args) {
-        int num,aux;
-        Scanner input= new Scanner(System.in);
-        num= input.nextInt();
-        aux=num/200;
-        num=num%200;
-        System.out.println(aux+"\t"+num);
+
+        Scanner input = new Scanner(System.in);
+
+
+
+        double saldoM, valorC, saldoCadames;
+
+
+//        System.out.println("Introduza o valor do saldo de Janeiro: ");
+//        saldoCadames= input.nextInt();
+//        System.out.println("Introduza o valor do saldo de Fevereiro: ");
+//        saldoCadames= saldoCadames + input.nextInt();
+//        System.out.println("Introduza o valor do saldo de Março: ");
+//        saldoCadames= saldoCadames + input.nextInt();
+//        System.out.println("Introduza o valor do saldo de Março: ");
+//        saldoCadames= saldoCadames + input.nextInt();
+//
+//        System.out.println(saldoCadames);
+//        saldoM =  saldoCadames /12;
+
+        System.out.println("Introduza o valor do saldo Médio");
+        saldoM = input.nextInt();
+
+        if (saldoM >= 0 && saldoM <=2000){
+
+        System.out.println("O saldo médio é de " +saldoM + " e o valor do crédito é 0" );
+        }
+
+        if (saldoM > 2000 && saldoM <=4000){
+
+            valorC = saldoM * 0.2;
+
+        System.out.println("O saldo médio é de " +saldoM + " e o valor do crédito é " + valorC );
+        }
+
+        if (saldoM > 4000 && saldoM <=6000){
+
+            valorC = saldoM * 0.3;
+
+            System.out.println("O saldo médio é de " +saldoM + " e o valor do crédito é " + valorC );
+        }
+
+        if (saldoM > 6000){
+
+            valorC = saldoM * 0.4;
+
+            System.out.println("O saldo médio é de " +saldoM + " e o valor do crédito é " + valorC );
+        }
     }
 }
